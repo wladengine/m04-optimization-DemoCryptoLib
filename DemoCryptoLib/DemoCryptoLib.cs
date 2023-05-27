e1bcd3bbf6b63821d9c0656980288b326e24876c
Vladislav Chikhira <Vladislav_Chikhira@epam.com>
2023-05-27 13:20:49 +0300

    return body for the function

--- DemoCryptoLib/DemoCryptoLib.cs
+++ DemoCryptoLib/DemoCryptoLib.cs
@@ -22,5 +22,18 @@ public class DemoCryptoLib
     }
 
     public static string GeneratePasswordHashUsingSaltOptimized(string passwordText, byte[] salt)
-        => GeneratePasswordHashUsingSalt(passwordText, salt);
+    {
+        const int iterate = 10000;
+        using var pbkdf2 = new Rfc2898DeriveBytes(passwordText, salt, iterate);
+
+        byte[] hash = pbkdf2.GetBytes(20);
+
+        var hashBytes = new byte[36];
+        Array.Copy(salt, 0, hashBytes, 0, 16);
+        Array.Copy(hash, 0, hashBytes, 16, 20);
+
+        string passwordHash = Convert.ToBase64String(hashBytes);
+
+        return passwordHash;
+    }
 }
